48776b202bb3409dfb43639e3e0b40f893820350
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-10 09:17:14 -0700

    pass down data and methods to components

--- village/src/App.js
+++ village/src/App.js
@@ -39,8 +39,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addFriend=/>
-        <Smurfs smurfs={this.state.smurfs} />
+        <SmurfForm addSmurf={this.addSmurf} />
+        <Smurfs smurfs={this.state.smurfs} deleteSmurf={this.deleteSmurf}  />
       </div>
     );
   }
