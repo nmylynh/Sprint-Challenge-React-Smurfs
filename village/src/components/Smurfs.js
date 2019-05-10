8a5fbf73561378aa17b8145ef2c88f96cd14e477
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-10 09:41:09 -0700

    deleteSmurf returned undefined, fixed it with this

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -16,7 +16,7 @@ class Smurfs extends Component {
                 age={smurf.age}
                 height={smurf.height}
                 key={smurf.id}
-                deleteSmurf={props.deleteSmurf}
+                deleteSmurf={this.props.deleteSmurf}
               />
             );
           })}
