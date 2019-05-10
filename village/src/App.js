4bcb9dbecb9966ed200e88ef41fdf74c0a577c6c
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-10 10:14:32 -0700

    import react, used container class

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
