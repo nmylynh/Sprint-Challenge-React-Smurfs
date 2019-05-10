471a1cf980c08c86eea5ab9359e5ed8d59c17f0d
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-10 09:06:38 -0700

    import axios

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -27,7 +27,11 @@ class App extends Component {
       })
       .catch(err => console.log(err))
   }
-  
+
+  deleteFriend = id => {
+    axios.delete
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
